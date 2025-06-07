4de84728574ac38e87b3e185b279530e2de79216
yxchen <yxchen0323@email.com>
2025-06-07 20:09:47 +0800

    feat: add initial TypeScript setup with example code and configuration

---
+++ Tutorial/TypeScript/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hello, TypeScript!");
+let age = 25;
+if (age > 18) {
+    console.log("You are an adult.");
+}
+//# sourceMappingURL=index.js.map
---
+++ Tutorial/TypeScript/src/index.ts
@@ -0,0 +1,5 @@
+console.log("Hello, TypeScript!");
+let age: number = 25;
+if (age > 18) {
+    console.log("You are an adult.");
+}
