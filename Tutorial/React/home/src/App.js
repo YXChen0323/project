182ad1e618cf43edc427353d598415990ab09158
yxchen <yxchen0323@email.com>
2025-06-13 10:36:46 +0800

    feat: add multiplication buttons to App component

--- Tutorial/React/home/src/App.js
+++ Tutorial/React/home/src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+const multbutton = () => {
+  var output =[];
+  for (let i = 1; i <= 10; i++) {
+    output.push(<button key={i} className="mult-button">{i}</button>);
+  }
+  return output;
+};
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +17,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 style={{fontSize: '100px',color:'red'}}>Welcome to React!</h1>
+        <h1 className='App-title'>Welcome to React!</h1>
+        <div className="mult-buttons">
+          {multbutton()}
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
